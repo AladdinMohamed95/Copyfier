f477cb7a95dc80673981b772035801b08bac6c8a
Aladdin Mohamed <3laa.abd.elhamid@gmail.com>
2024-11-06 10:36:25 +0200

    standalone

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "standalone",
+  // Optional: Enable static site generation if you're building a static site
+  target: "serverless", // If you're deploying on Vercel or AWS Lambda
+  reactStrictMode: true, // Optional: Enables React strict mode
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
